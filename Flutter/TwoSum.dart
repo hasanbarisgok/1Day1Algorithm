8cfb31067fd3b59b6e781ac1d3634b6679b7f9da
Hasan Barış GÖK <70600067+hasanbarisgok@users.noreply.github.com>
2022-08-27 00:02:40 +0300

    Update TwoSum.dart

--- Flutter/TwoSum.dart
+++ Flutter/TwoSum.dart
@@ -1,1 +1,12 @@
-
+void main() {
+  var listofNums = [1, 2, 3, 4];
+  int target = 6;
+  for (var a = 0; a < listofNums.length; a++) {
+    for (var b = a + 1; b < listofNums.length; b++) {
+      int sum = listofNums[a] + listofNums[b];
+      if (sum == target) {
+        print("${listofNums[a]},${listofNums[b]}");
+      }
+    }
+  }
+}
