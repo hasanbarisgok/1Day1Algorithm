96418b9ae9279c727a0ef27d156c181a54eb66c2
Hasan Barış GÖK <70600067+hasanbarisgok@users.noreply.github.com>
2022-08-26 23:50:56 +0300

    Update and rename twoSum to twoSum.py

---
+++ Pyton/twoSum.py
@@ -0,0 +1,11 @@
+#Problem Name: Two Sum <= LeetCode 
+#When sum of two numbers of nums list equal target, we're printing the two nums on terminal.
+
+nums = [1,2,3,4]
+target = 6
+newList = []
+for a in range(0,len(nums)):
+    for b in range(a+1,len(nums)):
+        sum = nums[a] + nums[b]
+        if sum == target:
+         print(f"{nums[a]},{nums[b]}")
