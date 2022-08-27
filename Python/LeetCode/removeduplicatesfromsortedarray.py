1eec4cc199f054870833fd2a5adcc5b5df47270d
Hasan Barış GÖK <70600067+hasanbarisgok@users.noreply.github.com>
2022-08-27 03:34:59 +0300

    Update removeduplicatesfromsortedarray.py

--- Python/LeetCode/removeduplicatesfromsortedarray.py
+++ Python/LeetCode/removeduplicatesfromsortedarray.py
@@ -3,7 +3,7 @@
 # So we'll solve the problem with only a original nums list.
 
 nums = [0,0,1,1,1,2,2,3,3,4]
-l = 1 #We've gave the 1 value for variable l. Because on other steps we'll use variable l both counter, and new duplicate value of the list. 
+l = 1 #We've gave the 1 value for variable l. Because we'll use variable l both counter, and new duplicate value of the list other steps. 
       #Thanks to this, we'll use the memory really low.
       #And our code will work really faster than others. 
       #You can look at my Challange details at the end of the line.(Memory Usage, Runtime etc.)
