789a549ca5b7b56211d210cde35fef3cef62a944
Hasan Barış GÖK <70600067+hasanbarisgok@users.noreply.github.com>
2022-08-27 03:30:15 +0300

    Update removeduplicatesfromsortedarray.py

--- Python/LeetCode/removeduplicatesfromsortedarray.py
+++ Python/LeetCode/removeduplicatesfromsortedarray.py
@@ -1,8 +1,10 @@
 # Remove Duplicates from Sorted Array <= LeetCode
-# Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory. So we'll solve the problem with only a original nums list.
+# Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory. 
+# So we'll solve the problem with only a original nums list.
 
 nums = [0,0,1,1,1,2,2,3,3,4]
-l = 1 #We've gave the 1 value for l variable. Because on other steps we'll use l variable both counter and new duplicate value of the list. Thanks to this, we'll use the memory really low.
+l = 1 #We've gave the 1 value for l variable. Because on other steps we'll use l variable both counter and new duplicate value of the list. 
+      #Thanks to this, we'll use the memory really low.
       #And our code will work really faster than others. You can see my Challange details. (Memory Usage, Runtime etc.)
 
 for a in range(1,len(nums)):
