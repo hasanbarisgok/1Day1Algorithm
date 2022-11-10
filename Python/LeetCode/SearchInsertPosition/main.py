e9da3c0aec8ce3444796b67ca21efd4bd1c81387
Hasan Barış GÖK <70600067+hasanbarisgok@users.noreply.github.com>
2022-11-11 02:00:44 +0300

    Create main.py

---
+++ Python/LeetCode/SearchInsertPosition/main.py
@@ -0,0 +1,9 @@
+from source import Position
+list = []
+for a in range(0,100):
+    list.append(a)
+
+new = Position(list,3)
+new = Position(list,76)
+new = Position(list,129)
+new = Position(list,-1)
