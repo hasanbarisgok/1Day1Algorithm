0ae343cd95fdff3ff5202cdc3fd647ca31bbdbf7
Hasan Barış GÖK <70600067+hasanbarisgok@users.noreply.github.com>
2022-11-13 23:27:39 +0300

    Update checkPrimeNum.py

--- Python/Personal/checkPrimeNum.py
+++ Python/Personal/checkPrimeNum.py
@@ -1,18 +1,32 @@
 class Prime(object):
     
     def __init__(self,num:int):
-        self.num = num
-        self.result : bool
-        self.count: int = 0
-        self.check_prime()
+        """Prime Class Constructor. 
+
+        Args:
+            num (int): The num whic will check.
+        """
+        self.num = num #The num whic will check.
+        self.result : bool #The instance variable $result uses for checking count num.
+        self.count: int = 0 #The instance variable $count uses for counting number of divisiors for self.num
+        self.check_prime() 
         print(self.return_check())
         
     def check_prime(self):
+        """The method counts number of divisiors.
+        """
         for num in range(2,self.num+1):
             if self.num % num == 0:
                 self.count += 1
     
     def return_check(self):
+        """The method checks $self.count value. If the value is less then 2 (self.count < 2) returns True for $self.result.
+        Else, returns False for $self.result.
+        
+        Returns:
+            _true_: The Num: {self.num} is not prime.
+            _false_: "The Num: {self.num} is prime."
+        """
         if self.count < 2 or self.num == 2:
                 self.result = True
         else:
@@ -28,3 +42,5 @@ class Prime(object):
 for new in range(0,101):
     new = Prime(new)
     print(30*"*")
+
+#HasanBarisGOK @ linkedin: linkedin/in/hasanbarisgok
